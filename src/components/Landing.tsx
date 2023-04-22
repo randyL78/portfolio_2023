8802928044e822c9a710e8f28c0586e11bd1267d
Randy Layne <randydavidl78@gmail.com>
2023-04-22 11:01:24 -0400

    Tweak Landing color and opacity

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Landing = () => {
   return (
     <div className="top-0 bottom-0 absolute start-0 end-0 landing bg-cover bg-center bg-fixed" >
-      <div className="top-0 bottom-0 absolute start-0 end-0 bg-blue-950 opacity-50"></div>
+      <div className="top-0 bottom-0 absolute start-0 end-0 bg-blue-900 opacity-50"></div>
     </div >
   )
 }
