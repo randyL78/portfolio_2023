e155ca8f1fadb482486607d7f901ad81e4bc044c
Randy Layne <randydavidl78@gmail.com>
2023-04-21 23:32:53 -0400

    Tweak Hero and background image styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="h-screen">
         <div className="text-center text-white absolute top-1/4 start-1/2 -translate-x-1/2 translate-y-1/2">
           <h1 className="text-6xl font-bold">Randy Layne</h1>
-          <hr className="my-4 opacity-25" />
+          <hr className="my-5 opacity-25" />
           <h2 className="text-2xl font-light">Software Developer</h2>
         </div>
       </div>
