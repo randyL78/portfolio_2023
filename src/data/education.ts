290928408a14bc2e5c986990287f42920dab78d7
Randy Layne <randydavidl78@gmail.com>
2023-04-23 14:06:31 -0400

    Add education experience at CVCC

--- src/data/education.ts
+++ src/data/education.ts
@@ -8,7 +8,7 @@ export const educations: Education[] = [
     location: 'Norfolk, VA',
     title: "Master's Degree",
     focus: 'Computer Science',
-    gpa: 3.86,
+    gpa: 3.82,
     description: 'The graduate level program at ODU for computer science places ' +
       'emphasis on computational thinking and cyber security. They back up these ' +
       'theoretical studies with collaborative hands-on projects. Additionally, my ' +
@@ -22,12 +22,24 @@ export const educations: Education[] = [
     location: 'Norfolk, VA',
     title: "Bachelor's Degree",
     focus: 'Computer Science, Cyber Security(Minor)',
-    gpa: 3.95,
+    gpa: 3.92,
     description: "Learned the fundamentals of Object Oriented Design, as well as " +
       "Algorithms and Data Structures. Mixed problem solving and critical thinking " +
       "with hands on coding in multiple languages including C, C++, Java, and " +
       "introductions to Rust. Learned about project life-cycles and created a team-built " +
       "prototype of software to address a real-world problem. Was a 4 time recipient of " +
       "the Dean's Perfect Scholar Award."
-  }
+  },
+  {
+    institution: 'Central Virginia Community College',
+    startDate: 'Aug 2017 ',
+    endDate: 'Dec 2019',
+    location: 'Lynchburg, VA',
+    title: "Associate's Degree",
+    focus: 'Business Administration with IT Specialization',
+    gpa: 3.89,
+    description: 'Focused on general studies and topics in business management. Took ' +
+      'deep dives into accounting systems, MS Office, software development, and IT skills.'
+  },
+
 ]
